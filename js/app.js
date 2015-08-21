1cb553420481094a31d432ec6d0a3f204eda1757
tuskerette <valeria.graffeo@gmail.com>
2015-08-21 16:58:26 -0400

    main Italian home page content completed

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-      $('.parallax').parallax();
+  $('.parallax').parallax();
 
-
-
- $('.button-collapse').sideNav({
+  $('.button-collapse').sideNav({
       menuWidth: 240, // Default is 240
       edge: 'left', // Choose the horizontal origin
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     }
   );
+
+
 
 });
 
