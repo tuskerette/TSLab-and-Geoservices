73cd540571c5ad191fe94c5cf0521eed8f0bb2b3
tuskerette <valeria.graffeo@gmail.com>
2015-08-21 15:38:02 -0400

    added images to sezioni sottili

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
+$(document).ready(function(){
+      $('.parallax').parallax();
+
+
+
  $('.button-collapse').sideNav({
       menuWidth: 240, // Default is 240
       edge: 'left', // Choose the horizontal origin
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     }
   );
+
+});
 
